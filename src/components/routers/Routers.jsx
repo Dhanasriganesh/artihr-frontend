73dc23adf739930c8bf3fcba02964b0f04c700f9
Ganesh <dhanasriganesh9@gmail.com>
2026-01-05 11:41:27 +0530

    Update frontend code

--- src/components/routers/Routers.jsx
+++ src/components/routers/Routers.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/Sign-in/Login'
 import ForgotPassword from '../pages/Sign-in/Forgot-Password'
 import Dashboard from '../pages/Dashboard/Dashboard'
-
 function Routers() {
   return (
     <Routes>
@@ -13,5 +12,4 @@ function Routers() {
     </Routes>
   )
 }
-
 export default Routers
